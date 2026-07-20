6e70e1050d89051d5463e166b1faa0f270bfd06f
Lautaro Giunta <lautogiunta@gmail.com>
2026-07-20 13:03:23 -0300

    creo crud localidad

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -5,12 +5,13 @@ import { rolRouter } from "./rol/rol.routes.js";
 import { especieRouter } from "./especie/especie.routes.js";
 import { razaRouter } from "./raza/raza.routes.js";
 import { usuarioRouter } from "./usuario/usuario.routes.js";
+import { localidadRouter } from "./localidad/localidad.routes.js";
 
 const app = express();
 
-app.use(express.json());
 
-//luego de los middlewares base
+app.use(express.json());//luego de los middlewares base
+
 app.use((req, res, next) => {
   RequestContext.create(orm.em, next);
 });
@@ -19,6 +20,7 @@ app.use("/api/roles", rolRouter);
 app.use("/api/especies", especieRouter);
 app.use("/api/razas", razaRouter);
 app.use("/api/usuarios", usuarioRouter);
+app.use("/api/localidad", localidadRouter)
 
 await syncSchema(); //no usar en produccion
 
---
+++ apps/backend/src/localidad/localidad.controller.ts
@@ -0,0 +1,60 @@
+import type { Request, Response } from "express";
+import { orm } from "../shared/db/orm.js";
+import { Localidad } from "./localidad.entity.js";
+
+const em = orm.em
+
+async function findAll(req: Request, res: Response) {
+  try{
+    const localidades = await em.find(Localidad,{})
+    res.status(200).json({message:'find all localidades', data:localidades})
+  } catch(error:any){
+    res.status(500).json({message: error.message})
+  }
+}
+
+async function findOne(req: Request, res: Response) {
+  try{
+    const id = Number(req.params.id)
+    const localidad = await em.findOneOrFail(Localidad, {id})
+    res.status(200).json({message:'found localidad', data: localidad})
+  }catch(error:any){
+    res.status(500).json({message: error.message})
+  }
+}
+
+async function add(req: Request, res: Response) {
+  try{
+    const localidad = em.create(Localidad, req.body)
+    await em.flush()
+    res.status(201).json({message:'localidad created', dat: localidad})
+  } catch(error:any){
+    res.status(500).json({message: error.message})
+  }
+}
+
+async function update(req: Request, res: Response) {
+  try{
+    const id = Number(req.params.id)
+    const localidad = em.getReference(Localidad, id)
+    em.assign(localidad, req.body)
+    await em.flush()
+    res.status(200).json({message: 'localidad modificada correctamente', data: localidad})
+  }catch(error:any){
+    res.status(500).json({message: error.message})
+  }
+}
+
+async function remove(req: Request, res: Response) {
+  try{
+    const id = Number(req.params.id)
+    const localidad = em.getReference(Localidad, id)
+    em.remove(localidad)
+    await em.flush()
+    res.status(200).json({message:'localidad deleted'})
+  }catch(error:any){
+    res.status(500).json({message: error.message})
+  }
+}
+
+export {findAll, findOne, add, update, remove}
